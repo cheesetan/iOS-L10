954583c63c5b8f0102f0154ec27a1e2d5c29cac2
Tristan <tristanchay123@gmail.com>
2023-09-15 21:14:30 +0800

    Initial Commit

---
+++ iOS Lesson 10 Github/iOS_Lesson_10_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Lesson_10_GithubApp.swift
+//  iOS Lesson 10 Github
+//
+//  Created by Tristan Chay on 15/9/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Lesson_10_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
